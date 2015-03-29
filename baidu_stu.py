aa1d1d3f890d1f3d14403d34bb63a4c6f251ec5d
Σndless <loli.con@qq.com>
2015-03-29 11:10:12 +0800

    Update baidu_stu.py

--- baidu_stu.py
+++ baidu_stu.py
@@ -13,8 +13,8 @@ UA = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/537.36 (KHTML,
 
 
 def baidu_stu_lookup(im):
-    url = "http://stu.baidu.com/n/image?fr=html5&needRawImageUrl=true&id=WU_FILE_0&name=233.png&type=image%2Fpng&lastModifiedDate=Mon+Mar+16+2015+20%3A49%3A11+GMT%2B0800+(CST)&size="
-im.save("./query_temp_img.png")
+    url = "http://stu.baidu.com/n/image?fr=html5&needRawImageUrl=true&id=WU_FILE_0&name=233.png&type=image%2Fpng&lastModifiedDate=Mon+Mar+16+2015+20%3A49%3A11+GMT%2B0800+(CST)&size="    
+    im.save("./query_temp_img.png")
     raw = open("./query_temp_img.png", 'rb').read()
     url += str(len(raw))
     req = urllib2.Request(url, raw, {'Content-Type': 'image/png', 'User-Agent': UA})
