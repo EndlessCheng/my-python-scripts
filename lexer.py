f7f9f2914a6dbe2e38a516bec25c77b11a62fc71
Σndless <loli.con@qq.com>
2015-04-05 16:00:23 +0800

    Update lexer.py

--- lexer.py
+++ lexer.py
@@ -6,16 +6,16 @@ import string
 
 
 class Lexer:
-        def __init__(self, file_name):
-                self.file = open(file_name)
-        
-        def read_line(self):
-                for line in self.file.readlines():
-                        for m in re.compile(r"\s*("
+    def __init__(self, file_name):
+        self.file = open(file_name)
+
+    def read_line(self):
+        for line in self.file.readlines():
+            for m in re.compile(r"\s*("
                                 r"(//.*)|"  # commit
                                 r"([0-9]+)|"  # number
                                 r"(\"(\\\"|\\\\|\\n|[^\"])*\")|"  # string
                                 r"[A-Z_a-z][A-Z_a-z0-9]*|"
                                 r"<=|==|>=|&&|\|\||[%s])" % re.escape(string.punctuation)).findall(line):
-                                yield m  # .decode('string-escape')
-                yield (r'\n', r'', r'', r'', r'')
+                yield m  # .decode('string-escape')
+        yield (r'\n', r'', r'', r'', r'')
