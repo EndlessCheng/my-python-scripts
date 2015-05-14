914a456b7630fe971a346aa58c8255a1f0df8da6
Σndless <loli.con@qq.com>
2015-05-14 19:13:30 +0800

    Create generator_magic_square.py

---
+++ generator_magic_square.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+
+
+def generator_odd(size, start=1):
+    array = [x[:] for x in [[0] * size] * size]
+    x, y = 0, size >> 1
+    for i in range(start, size * size + start):
+        array[x][y] = i
+        tx, ty = (x - 1 + size) % size, (y + 1) % size
+        if array[tx][ty] != 0:
+            x = (x + 1) % size
+        else:
+            x, y = tx, ty
+    return array
+
+
+def generator_double_even(size):
+    gap1 = size >> 2
+    gap2 = size - gap1
+
+    array = []
+    cnt = 1
+    recnt = size * size
+    for i in range(size):
+        tarray = []
+        if i < gap1 or i >= gap2:
+            for j in range(size):
+                tarray.append(recnt if gap1 <= j < gap2 else cnt)
+                cnt += 1
+                recnt -= 1
+        else:
+            for j in range(size):
+                tarray.append(cnt if gap1 <= j < gap2 else recnt)
+                cnt += 1
+                recnt -= 1
+        array.append(tarray)
+    return array
+
+
+def generator_double_odd(size):
+    half = size >> 1
+    k = half >> 1
+
+    array_a = generator_odd(half)
+    array_b = generator_odd(half, 3 * half * half + 1)
+    for i in range(half):
+        for j in range(half >> 1):
+            array_a[i][j], array_b[i][j] = array_b[i][j], array_a[i][j]
+    for j in range(half - 1):
+        array_a[k][j], array_b[k][j] = array_b[k][j], array_a[k][j]
+
+    array_c = generator_odd(half, 2 * half * half + 1)
+    array_d = generator_odd(half, half * half + 1)
+    for i in range(half):
+        for j in range(2, k + 1):
+            array_c[i][j], array_d[i][j] = array_d[i][j], array_c[i][j]
+
+    array = []
+    for i in range(half):
+        array_a[i].extend(array_c[i])
+        array.append(array_a[i])
+    for i in range(half):
+        array_b[i].extend(array_d[i])
+        array.append(array_b[i])
+    return array
+
+
+def print_array(array):
+    num_len = len(str(len(array) * len(array)))
+    for i in array:
+        for j in i:
+            print "%*d" % (num_len, j),
+        print
+
+
+if '__main__' == __name__:
+    while True:
+        size = input()
+        assert isinstance(size, int) and size >= 3
+
+        if size & 1:
+            result = generator_odd(size)
+        elif size & 3:
+            result = generator_double_odd(size)
+        else:
+            result = generator_double_even(size)
+
+        print_array(result)
