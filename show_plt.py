dd9f1e84da2812d351306f750cb2d407910348e9
Σndless <loli.con@qq.com>
2017-07-15 16:03:37 +0800

    显示代码增长进度.py

---
+++ show_plt.py
@@ -0,0 +1,21 @@
+import os
+import datetime as dt
+import matplotlib.pyplot as plt
+
+x = []
+y = []
+
+with open(os.path.expanduser('~') + '/code_line_log.txt') as f:
+    lines = f.readlines()
+    for line in lines:
+        splits = line.split('=')
+        if len(splits) != 2:
+            break
+        time = splits[0].strip()
+        code_lines = splits[1].strip()
+        x.append(dt.datetime.strptime(time, '%Y-%m-%d %H:%M:%S').date())
+        y.append(int(code_lines))
+
+plt.plot(x, y)
+plt.gcf().autofmt_xdate()
+plt.show()
