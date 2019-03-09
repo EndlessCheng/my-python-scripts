ebb6fb22730059d46f0a6c858b251ab05dbf2f8f
Endless <loli.con@qq.com>
2019-03-09 11:27:09 +0800

    歌词精简

--- lyrics.py
+++ lyrics.py
@@ -1,7 +1,7 @@
 import wx
 
 
-def reset_on_click(event):
+def modify_on_click(event):
     text = text_ctrl.GetValue()
 
     # remove empty line
@@ -10,7 +10,19 @@ def reset_on_click(event):
     # remove translate
     text = text[::2]
 
-    text_ctrl.SetValue('\n'.join(text))
+    text = '\n'.join(text)
+
+    text_ctrl.SetValue(text)
+
+    if wx.TheClipboard.Open():
+        data_obj = wx.TextDataObject()
+        data_obj.SetText(text)
+        wx.TheClipboard.SetData(data_obj)
+        wx.TheClipboard.Close()
+
+
+def clear_on_click(event):
+    text_ctrl.Clear()
 
 
 if __name__ == '__main__':
@@ -20,18 +32,29 @@ if __name__ == '__main__':
     w, h = 600, 480
     screen_width, screen_height = wx.GetDisplaySize()
     frame = wx.Frame(None, title='歌词精简', size=(w, h), pos=(screen_width - w, screen_height - h))
+
     panel = wx.Panel(frame)
 
+    # 组件
     text_ctrl = wx.TextCtrl(panel, style=wx.TE_MULTILINE)
 
-    reset_button = wx.Button(panel, label='精简')
-    reset_button.Bind(wx.EVT_BUTTON, reset_on_click)
+    modify_button = wx.Button(panel, label='精简')
+    modify_button.Bind(wx.EVT_BUTTON, modify_on_click)
+
+    clear_button = wx.Button(panel, label='清空')
+    clear_button.Bind(wx.EVT_BUTTON, clear_on_click)
+
+    # 结构
+    vbox = wx.BoxSizer(wx.VERTICAL)
+    vbox.Add(modify_button, flag=wx.EXPAND | wx.ALL, border=5)
+    vbox.Add(clear_button, flag=wx.EXPAND | wx.ALL, border=5)
 
     hbox = wx.BoxSizer()
     hbox.Add(text_ctrl, proportion=3, flag=wx.EXPAND)
-    hbox.Add(reset_button, proportion=1, flag=wx.EXPAND, border=5)
+    hbox.Add(vbox, proportion=1, flag=wx.EXPAND)
 
     panel.SetSizer(hbox)
 
+    #
     frame.Show()
     app.MainLoop()
